76941eb18ad22b19bfa91d98f8d3b74b33585cde
Edgar Cardona <edgar.cardona7783@gmail.com>
2022-03-10 14:46:18 -0600

    home_screen.dart: added Components Options

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,12 +5,46 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<String> titulosComponentes = [
+      'ListView Screen',
+      'Card Screen',
+      'Alert Screen',
+      'Avatar Screen',
+      'Container Screen',
+      'Text inputs Screen',
+      'Sliders Screen',
+      'List Builder Screen'
+    ];
+
+    final List<IconData> iconos = [
+      Icons.list_rounded,
+      Icons.card_giftcard_rounded,
+      Icons.warning_rounded,
+      Icons.login_rounded,
+      Icons.info_rounded,
+      Icons.text_format_rounded,
+      Icons.slideshow_rounded,
+      Icons.social_distance_rounded
+    ];
+
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-          onPressed: () {}, elevation: 0, backgroundColor: Colors.indigo),
-      body: const Center(
-        child: Text('Utilidades'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Componentes en Flutter'),
+          centerTitle: true,
+        ),
+        body: ListView.separated(
+          separatorBuilder: (context, index) {
+            return const Divider(
+              color: Colors.indigo,
+            );
+          },
+          itemCount: titulosComponentes.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: Icon(iconos[index]),
+              title: Text(titulosComponentes[index]),
+            );
+          },
+        ));
   }
 }
